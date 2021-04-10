c1e48c657e903fde5bb907f0870066284ab34892
Jorge Serrano <jserrano@dadep.gov.co>
2021-04-10 11:04:58 -0500

    Refactor: Se agrega la variable String apellido

--- variables/bin/variables.dart
+++ variables/bin/variables.dart
@@ -2,8 +2,9 @@ import 'package:variables/variables.dart' as variables;
 
 
 void main() {
-  var nombre = "juan";
-  var edad = 23;
-  print( nombre + " " + edad.toString());
+  var nombre = "Jorge";
+  String apellido = "Serrano";
+  var edad = 30;
+  print( nombre + " " + apellido + " " + edad.toString());
 
 }
