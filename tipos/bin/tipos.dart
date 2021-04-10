262b1d7ead473db8b0189c163972bb55e7a96fe0
Jorge Serrano <jserrano@dadep.gov.co>
2021-04-10 11:42:56 -0500

    Feature: Se realizan pruebas de tipos de variables

---
+++ tipos/bin/tipos.dart
@@ -0,0 +1,15 @@
+import 'package:tipos/tipos.dart' as tipos;
+
+void main(List<String> arguments) {
+
+
+  int edad =-52;
+  double estatura = 1.80;
+
+  if(edad.isNegative){
+    print('ES NEGATIVO');
+    print(edad.abs());
+  } else {
+    print(edad.abs());
+  }
+}
---
+++ tipos/test/tipos_test.dart
@@ -0,0 +1,8 @@
+import 'package:tipos/tipos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
